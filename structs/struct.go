0ae5914acd4f1f9644383ebb0599dabef739e6de
skillsworld46 <skillsworld46@gmail.com>
2025-03-17 18:22:09 +0800

    structs, methods, and interfaces

---
+++ structs/struct.go
@@ -0,0 +1,28 @@
+package structs
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Height float64
+	Width  float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Height + r.Width)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
