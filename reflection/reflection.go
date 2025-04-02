c6a957f45690c31efb6a5a25b4f33bcc8eb688ea
skillsworld46 <skillsworld46@gmail.com>
2025-04-02 21:45:46 +0800

    reflection

---
+++ reflection/reflection.go
@@ -0,0 +1,34 @@
+package reflection
+
+import (
+	"reflect"
+)
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+type Profile struct {
+	Age  int
+	City string
+}
+
+func walk(x interface{}, fn func(input string)) {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			walk(field.Interface(), fn)
+		}
+	}
+
+}
---
+++ reflection/relection_test.go
@@ -0,0 +1,76 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{
+				"Chris",
+			},
+			[]string{"Chris"},
+		},
+		{
+			"struct with two string field",
+			struct {
+				Name string
+				City string
+			}{
+				"Chris",
+				"London",
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{
+				"Chris",
+				18,
+			},
+			[]string{"Chris"},
+		},
+		{
+			"nested fields",
+			Person{
+				"Chris",
+				Profile{18, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"pointer to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v , want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
