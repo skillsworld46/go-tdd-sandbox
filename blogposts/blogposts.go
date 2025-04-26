8382f1831f9aceb0ed39c30c52bb52fe278b74ca
skillsworld46 <skillsworld46@gmail.com>
2025-04-27 02:30:26 +0800

    reading files

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -2,18 +2,31 @@ package blogposts
 
 import (
 	"io/fs"
-	"testing/fstest"
 )
 
-type Post struct {
+// NewPostsFromFS returns a collection of blog posts from a file system. If it does not conform to the format then it'll return an error
+func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
+	var posts []Post
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f)
+		if err != nil {
+			return nil, err //todo: needs clarification, should we totally fail if one file fails? or just ignore?
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
 }
 
-func NewPostsFromFs(fileSystem fstest.MapFS) []Post {
-	dir, _ := fs.ReadDir(fileSystem, ".")
-	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
 	}
+	defer postFile.Close()
 
-	return posts
+	return newPost(postFile)
 }
